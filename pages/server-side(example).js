4a5e155f4f51a2a7228012e2e24afd3fb2a50bae
JamaCodes <jama.avwork@gmail.com>
2022-11-10 13:10:30 -0600

    quickstart tutorial

--- pages/server-side(example).js
+++ pages/server-side(example).js
@@ -33,12 +33,21 @@ export default function Home({ countries }) {
         </div>
       </main>
 
-      
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
     </div>
   );
 }
-
-export async function getStaticProps() {
+//serverside rendering
+export async function getServerSideProps() {
   const { data } = await client.query({
     query: gql`
       query Countries {
